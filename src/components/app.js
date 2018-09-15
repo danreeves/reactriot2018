34f6a565619918955b8db4277d9479860e9cc7db
Dan Reeves <hey@danreev.es>
2018-09-15 18:19:33 +0100

    cool logo

--- src/components/app.js
+++ src/components/app.js
@@ -4,6 +4,20 @@ import Login from "./login";
 import Game from "./game";
 import UserState from "../state/user";
 
+const logoStyle = {
+  position: "absolute",
+  fontFamily: "IMPACT",
+  top: "40%",
+  backgroundColor: "rgba(0,0,0,0.5)",
+  webkitBackgroundClip: "text",
+  color: "transparent",
+  textShadow: "#00807f87 3px 5px 1px",
+  fontSize: "100px",
+  width: "100%",
+  userSelect: "none",
+  textAlign: "center"
+};
+
 export default class App extends React.Component {
   render() {
     return (
@@ -13,7 +27,12 @@ export default class App extends React.Component {
             if (!user.state.isLoggedIn) {
               return <Login />;
             }
-            return <Game />;
+            return (
+              <React.Fragment>
+                <span style={logoStyle}>DAN Corp.</span>
+                <Game />
+              </React.Fragment>
+            );
           }}
         </Subscribe>
       </Provider>
