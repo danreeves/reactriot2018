0383fc8313ce565b055d1970d64d1e566a889a41
Dan Reeves <hey@danreev.es>
2018-09-15 20:57:33 +0100

    loading fix

--- src/components/loader.js
+++ src/components/loader.js
@@ -5,7 +5,8 @@ const bar = {
   height: "25px",
   width: "100%",
   border: "1px solid black",
-  overflow: "hidden"
+  overflow: "hidden",
+  margin: "0.5rem 0"
 };
 
 const section = {
@@ -27,7 +28,7 @@ export default function Loader(props) {
   const loadbar = {
     ...loader,
     left: props.loading ? "100%" : "0%",
-    transition: (props.loading ? props.time : "0") + "s all linear"
+    transition: (props.loading ? props.time / 1000 : "0") + "s all linear"
   };
   return (
     <div style={bar}>
