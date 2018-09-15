74ce2ac0b544cd72b3b9f37189facd3b8823a9d6
Daniel Lewis <dantheorange@googlemail.com>
2018-09-15 15:10:30 +0100

    styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './components/app.js'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app.js";
 
-const root = document.querySelector('#app')
+const root = document.querySelector("#app");
 
-ReactDOM.render(<App />, root)
+ReactDOM.render(<App />, root);
