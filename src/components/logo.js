4eb2cc3b01422f684fad8a3e0864c71e81dabe95
Dan Reeves <hey@danreev.es>
2018-09-16 21:14:37 +0100

    fix logo?

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,10 +1,15 @@
 import React from "react";
 
-const logoStyle = {
+const wrapperStyle = {
   position: "absolute",
+  top: "40%",
+  width: "100%",
+  textAlign: "center"
+};
+
+const logoStyle = {
   fontFamily:
     "Impact,Haettenschweiler,Franklin Gothic Bold,Charcoal,Helvetica Inserat,Bitstream Vera Sans Bold,Arial Black,sans serif",
-  top: "40%",
   backgroundColor: "rgba(0,0,0,0.5)",
   WebkitBackgroundClip: "text",
   color: "transparent",
@@ -17,19 +22,22 @@ const logoStyle = {
 
 const taglineStyle = {
   ...logoStyle,
-  top: "calc(40% + 120px)",
   fontFamily: "Brush Script MT,cursive",
-  fontSize: "1.3rem"
+  fontSize: "1.3rem",
+  bottom: "30px",
+  position: "absolute"
 };
 
 export default function Logo() {
   return (
-    <React.Fragment>
-      <div style={logoStyle}>
+    <div id="1" style={wrapperStyle}>
+      <div id="2" style={logoStyle}>
         <span style={{ fontSize: "1.5em", verticalAlign: "middle" }}>❏</span>
         DANCorp.
       </div>
-      <div style={taglineStyle}>Never forget who you're working for...</div>
-    </React.Fragment>
+      <div id="3" style={taglineStyle}>
+        Never forget who you're working for...
+      </div>
+    </div>
   );
 }
