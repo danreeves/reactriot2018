a680dd7fe417ae994b7c90e251e7d032601bbaff
Daniel Lewis <dantheorange@googlemail.com>
2018-09-16 21:18:49 +0100

    I did names

--- src/state/game.js
+++ src/state/game.js
@@ -8,35 +8,35 @@ export default class Game extends Container {
     clickers: [
       {
         time: 100,
-        name: "ello",
+        name: "Spanners",
         value: 1,
         unlockedAt: 0,
         upgradeCost: (value, count) => value * 1.21 ** count
       },
       {
         time: 3500,
-        name: "lol",
+        name: "Wrenches",
         value: 40,
         unlockedAt: 25,
         upgradeCost: (value, count) => value * 1.18 ** count
       },
       {
         time: 8000,
-        name: "lol",
+        name: "Logans",
         value: 90,
         unlockedAt: 250,
         upgradeCost: (value, count) => value * 1.16 ** count
       },
       {
         time: 12000,
-        name: "lol",
+        name: "Meat",
         value: 135,
         unlockedAt: 1500,
         upgradeCost: (value, count) => value * 1.15 ** count
       },
       {
         time: 20000,
-        name: "lol",
+        name: "Spiders!",
         value: 240,
         unlockedAt: 5000,
         upgradeCost: (value, count) => value * 1.14 ** count
